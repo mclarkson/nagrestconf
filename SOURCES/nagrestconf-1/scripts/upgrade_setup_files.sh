ddd6ad8739dbc51880643f89dbcfa9f1982cdc49
mclarkson <mclarkson@2386a10c-f648-4a37-a1b2-c483300251ba>
2012-11-05 12:32:06 +0000

    Added more nagios directives to the REST api. Timeperiods now complete (Fixes).

--- SOURCES/nagrestconf-1/scripts/upgrade_setup_files.sh
+++ SOURCES/nagrestconf-1/scripts/upgrade_setup_files.sh
@@ -3,131 +3,58 @@
 # For WWWUSER and NAG_OBJ_DIR
 . /etc/nagrestconf/nagctl.conf
 
-# Add columns for CONTACTS
-
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_contacts.setup`; do 
+append_commas()
+{
+    # uses globals
 
     OLDFILE="$FILE.old"
     mv $FILE $OLDFILE
     :>$FILE
+    numcommaswanted=$((NUMCOLS-1))
     while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 24 ]]; then
-            a=$((25-$numcommas))
+        numcommas=`echo -n "$line" | sed "s/[^,]//g" | wc -c`
+        if [[ $numcommas != $numcommaswanted ]]; then
+            a=$(($numcommaswanted-$numcommas))
             echo "$line`printf %${a}s | tr " " ,`" >>$FILE
         else
             echo "$line" >>$FILE
         fi
     done <$OLDFILE
     chown $WWWUSER: $FILE
+}
 
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_contacts.setup`; do
+    NUMCOLS=24
+    append_commas
 done
 
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_hosts.setup`; do 
-
-    OLDFILE="$FILE.old"
-    mv $FILE $OLDFILE
-    :>$FILE
-    while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 46 ]]; then
-            a=$((47-$numcommas))
-            echo "$line`printf %${a}s | tr " " ,`" >>$FILE
-        else
-            echo "$line" >>$FILE
-        fi
-    done <$OLDFILE
-    chown $WWWUSER: $FILE
-
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_hosts.setup`; do
+    NUMCOLS=46
+    append_commas
 done
 
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_hosttemplates.setup`; do 
-
-    OLDFILE="$FILE.old"
-    mv $FILE $OLDFILE
-    :>$FILE
-    while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 39 ]]; then
-            a=$((40-$numcommas))
-            echo "$line`printf %${a}s | tr " " ,`" >>$FILE
-        else
-            echo "$line" >>$FILE
-        fi
-    done <$OLDFILE
-    chown $WWWUSER: $FILE
-
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_hosttemplates.setup`; do
+    NUMCOLS=39
+    append_commas
 done
 
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_services*.setup`; do 
-
-    OLDFILE="$FILE.old"
-    mv $FILE $OLDFILE
-    :>$FILE
-    while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 46 ]]; then
-            a=$((47-$numcommas))
-            echo "$line`printf %${a}s | tr " " ,`" >>$FILE
-        else
-            echo "$line" >>$FILE
-        fi
-    done <$OLDFILE
-    chown $WWWUSER: $FILE
-
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_services*.setup`; do
+    NUMCOLS=46
+    append_commas
 done
 
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_hostgroups.setup`; do 
-
-    OLDFILE="$FILE.old"
-    mv $FILE $OLDFILE
-    :>$FILE
-    while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 7 ]]; then
-            a=$((8-$numcommas))
-            echo "$line`printf %${a}s | tr " " ,`" >>$FILE
-        else
-            echo "$line" >>$FILE
-        fi
-    done <$OLDFILE
-    chown $WWWUSER: $FILE
-
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_hostgroups.setup`; do
+    NUMCOLS=8
+    append_commas
 done
 
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_servicegroups.setup`; do 
-
-    OLDFILE="$FILE.old"
-    mv $FILE $OLDFILE
-    :>$FILE
-    while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 7 ]]; then
-            a=$((8-$numcommas))
-            echo "$line`printf %${a}s | tr " " ,`" >>$FILE
-        else
-            echo "$line" >>$FILE
-        fi
-    done <$OLDFILE
-    chown $WWWUSER: $FILE
-
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_servicegroups.setup`; do
+    NUMCOLS=8
+    append_commas
 done
 
-for FILE in `ls $NAG_OBJ_DIR/*/setup/*_timeperiods.setup`; do 
-
-    OLDFILE="$FILE.old"
-    mv $FILE $OLDFILE
-    :>$FILE
-    while read line; do
-        numcommas=`echo "$line" | sed "s/[^,]//g" | wc -c`
-        if [[ $numcommas != 6 ]]; then
-            a=$((7-$numcommas))
-            echo "$line`printf %${a}s | tr " " ,`" >>$FILE
-        else
-            echo "$line" >>$FILE
-        fi
-    done <$OLDFILE
-    chown $WWWUSER: $FILE
-
+for FILE in `ls $NAG_OBJ_DIR/*/setup/*_timeperiods.setup`; do
+    NUMCOLS=6
+    append_commas
 done
 
