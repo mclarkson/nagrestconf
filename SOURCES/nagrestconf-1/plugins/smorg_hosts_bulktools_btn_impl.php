94f33728d0f62f35b7da481d85ee1ca824aef90a
Mark Clarkson <mark.clarkson@smorg.co.uk>
2013-12-10 11:26:45 +0000

    Implemented Bulk Tools 'Refresh' tab.

--- SOURCES/nagrestconf-1/plugins/smorg_hosts_bulktools_btn_impl.php
+++ SOURCES/nagrestconf-1/plugins/smorg_hosts_bulktools_btn_impl.php
@@ -90,9 +90,10 @@
         print ' var active = $("#bulktoolstabs").tabs("option","active");';
         print ' var form;';
         print ' switch(active){'.
-              #'  case 0: form="#hstbulkform1"; break;'.
               '  case 0: form="#hstbulkform1"; break;'.
               '  case 1: form="#hstbulkform2"; break;'.
+              '  case 2: form="#hstbulkform3"; break;'.
+              '  case 3: form="#hstbulkform4"; break;'.
               ' };';
         print ' var query_string=$(form).serialize();';
         print ' query_string+="&active_tab="+active;';
@@ -233,24 +234,13 @@
         ###:TABS
         print '<div id="bulktoolstabs">';
         print '<ul>';
-        #print '<li><a href="#hstadd"><span>Add Hosts</span></a></li>';
         print '<li><a href="#hstmodify"><span>Modify Hosts</span></a></li>';
         print '<li><a href="#hstdelete"><span>Delete Hosts</span></a></li>';
+        print '<li><a href="#hstadd"><span>Add Hosts</span></a></li>';
+        print '<li><a href="#hstrefresh"><span>Refresh Hosts</span></a></li>';
         print '</ul>';
 
         ###:TAB1
-        #print '<div id="hstadd">';
-        #print '<form id="hstbulkform1" '.
-        #      'name="hstbulkform1" method="get"';
-        #print ' action="/nagrestconf/'.SCRIPTNAME.'?tab='.$id.'&sbulkapply=1';
-        #print '">';
-        #print '<fieldset>';
-        #print '<p><br />&nbsp;TODO - UNIMPLEMENTED</p>';
-        #print '</fieldset>';
-        #print '</form>';
-        #print '</div>';
-
-        ###:TAB2
         print '<div id="hstmodify">';
         print '<form id="hstbulkform1" '.
               'name="hstbulkform1" method="get"';
@@ -275,6 +265,7 @@
             print '<option value="ipaddress">ipaddress</option>';
             #print '<option value="activechecks">activechecks</option>';
             print '<option value="maxcheckattempts">maxcheckattempts</option>';
+            print '<option value="servicesets">servicesets</option>';
             print '<option value="disable">disable</option>';
         print '</select>';
         print '</p>';
@@ -312,6 +303,11 @@
         #      '  if( selectedaction != "remove" ) $("#textp").show();'.
         #      '  $("#tristatep").hide();'.
         #      '}';
+        print 'if( selected == "servicesets" ){'.
+              '  $("#chactionp").show();'.
+              '  if( selectedaction != "remove" ) $("#textp").show();'.
+              '  $("#tristatep").hide();'.
+              '}';
         print 'if( selected == "maxcheckattempts" ){'.
               '  $("#chactionp").show();'.
               '  if( selectedaction != "remove" ) $("#textp").show();'.
@@ -363,7 +359,7 @@
         print '</form>';
         print '</div>';
 
-        ###:TAB3
+        ###:TAB2
         print '<div id="hstdelete">';
         print '<form id="hstbulkform2" '.
               'name="hstbulkform2" method="get"';
@@ -374,16 +370,58 @@
         $s=""; if( $size != 1 ) $s="s";
         print 'Pressing \'Apply Changes\' will DELETE '.$size." host$s.";
         print '</p>';
+        print '<p style="text-align: center; padding-top: 14px;" >';
+        print '<input type="checkbox" id="bulkdelhosts"';
+        print ' name="bulkdelhosts" ';
+        print ' style="display: inline-block; vertical-align: middle;';
+        print ' margin: 0 0 0 0; padding-top: 0px;" />';
+        print '<label for="bulkdelhosts" style="display: inline-block;';
+        print ' vertical-align: middle; float: none; width: auto;';
+        print ' padding-left: 6px; margin: 0 0 0 0;';
+        print ' padding-top: 0px;">Click to confirm deletion of ';
+        print ' '.$size.' host'.$s.'.</label>';
+        print '</p>';
         print '</fieldset>';
         print '</form>';
         print '</div>';
+
+        ###:TAB3
+        print '<div id="hstadd">';
+        print '<form id="hstbulkform3" '.
+              'name="hstbulkform3" method="get"';
+        print ' action="/nagrestconf/'.SCRIPTNAME.'?tab='.$id.'&sbulkapply=1';
+        print '">';
+        print '<fieldset>';
+        print '<p><br />&nbsp;TODO - UNIMPLEMENTED</p>';
+        print '</fieldset>';
+        print '</form>';
+        print '</div>';
+
+        ###:TAB4
+        print '<div id="hstrefresh">';
+        print '<form id="hstbulkform4" '.
+              'name="hstbulkform4" method="get"';
+        print ' action="/nagrestconf/'.SCRIPTNAME.'?tab='.$id.'&sbulkapply=1';
+        print '">';
+        print '<fieldset>';
+        print '<p style="font-weight: bold;text-align: center;" id="willaffect">';
+        $s=""; if( $size != 1 ) $s="s";
+        print 'Pressing \'Apply Changes\' will refresh '.$size." host$s.";
+        print '</p>';
+        print '<p style="padding-top: 10px; text-align: center;">';
+        print 'Service sets will be reapplied to the selected hosts.';
+        print '</p>';
+        print '</fieldset>';
+        print '</form>';
+        print '</div>';
+
+        ###:TABS DIV END
         print '</div>';
 
         print '<script>';
         print '$( "#bulktoolstabs" ).tabs();';
         print '</script>';
 
-        # END FIELDSET
         print '<hr />';
         print '<p>Log output:</p>';
         print '<textarea id="sbulktextarea" wrap="logical"';
@@ -416,6 +454,7 @@
             $field == "contacts" ||
             $field == "ipaddress" ||
             $field == "maxcheckattempts" ||
+            $field == "servicesets" ||
             $field == "contactgroups"
         ) {
 
@@ -597,6 +636,7 @@
             $field == "contacts" ||
             $field == "ipaddress" ||
             $field == "maxcheckattempts" ||
+            $field == "servicesets" ||
             $field == "contactgroups"
         ) {
 
@@ -696,6 +736,13 @@
     function rest_delete_hosts( $query_str ) {
     # ------------------------------------------------------------------------
 
+        if( ! isset( $query_str["bulkdelhosts"] ) ) {
+            $retval["message"] = "Error: Deletion was not confirmed.";
+            $retval["code"] = "400";
+            print( json_encode( $retval ) );
+            exit( 0 );
+        }
+
         $qs = $_SESSION['query_str'];
         $hgfilter = $qs['hgfilter'];
         if( empty( $hgfilter ) || $hgfilter == "all" ) {
@@ -774,6 +821,107 @@
     }
 
     # ------------------------------------------------------------------------
+    function rest_refresh_hosts( $query_str ) {
+    # ------------------------------------------------------------------------
+
+        $qs = $_SESSION['query_str'];
+        $hgfilter = $qs['hgfilter'];
+        if( empty( $hgfilter ) || $hgfilter == "all" ) {
+            $a = get_and_sort_hosts( $sort="name" );
+        } else {
+            $a = get_and_sort_hosts( $sort="name", $filter=$hgfilter,
+                $column=5 );
+        }
+
+        $n = 0;
+
+        foreach( $a as $item ) {
+            $n++;
+            $svcs = get_and_sort_services( $item["name"] );
+
+            # Save host details
+            $request = new \RestRequest(
+            RESTURL.'/show/hosts?json={"folder":"'.FOLDER.'",'.
+            '"column":"1","filter":"'.$item["name"].'"}', 'GET');
+            set_request_options( $request );
+            $request->execute();
+            $slist = json_decode( $request->getResponseBody(), true );
+
+            $new_qs = array();
+            $new_qs["folder"] = FOLDER;
+            foreach( $slist[0] as $item2 ) {
+                foreach( $item2 as $key => $val ) {
+                    $new_qs[$key] = $val; 
+                }
+            }
+            $newhostjson = json_encode( $new_qs );
+
+            # Delete host
+            if( sizeof($svcs) > 0 ) { 
+                $option = array();
+                $option["name"] = $item["name"];
+                $option["svcdesc"] = '.*';
+                $option["folder"] = FOLDER;
+                $json = json_encode( $option );
+                $request = new \RestRequest(
+                  RESTURL.'/delete/services',
+                  'POST',
+                  'json='.$json
+                );
+                set_request_options( $request );
+                $request->execute();
+                $slist = json_decode( $request->getResponseBody(), true );
+                $resp = $request->getResponseInfo();
+                if( $resp["http_code"] != 200 ) break;
+                ### Check $slist->http_code ###
+            }
+
+            $option = array();
+            $option["name"] = $item["name"];
+            $option["folder"] = FOLDER;
+            $json = json_encode( $option );
+            # Do the REST add host request
+            $request = new \RestRequest(
+              RESTURL.'/delete/hosts',
+              'POST',
+              'json='.$json
+            );
+            set_request_options( $request );
+            $request->execute();
+            $slist = json_decode( $request->getResponseBody(), true );
+            $resp = $request->getResponseInfo();
+            if( $resp["http_code"] != 200 ) break;
+
+            # Add host
+
+            # Do the REST add host request
+            $request2 = new \RestRequest(
+              RESTURL.'/add/hosts',
+              'POST',
+              'json='.$newhostjson
+            );
+            set_request_options( $request2 );
+            $request2->execute();
+            $slist = json_decode( $request2->getResponseBody(), true );
+
+            foreach( $slist as $slist_item )
+                $list .= "$n. Refreshing " . $item["name"] . 
+                          " : " . $slist_item . "\n";
+
+            $resp = $request->getResponseInfo();
+            if( $resp["http_code"] != 200 ) break;
+        }
+
+        # Return json
+        $retval = array();
+        $retval["message"] = $list;
+        $retval["code"] = $resp["http_code"];
+        print( json_encode( $retval ) );
+
+        exit(0);
+    }
+
+    # ------------------------------------------------------------------------
     function bulk_modify_hosts_using_REST( ) {
     # ------------------------------------------------------------------------
 
@@ -784,6 +932,12 @@
 
         if( $query_str["active_tab"] == 1 )
             rest_delete_hosts( $query_str );
+
+        #if( $query_str["active_tab"] == 2 )
+        #    rest_delete_hosts( $query_str );
+
+        if( $query_str["active_tab"] == 3 )
+            rest_refresh_hosts( $query_str );
 
         exit( 0 ); # <- Stop here - bypass any other actions.
     }
