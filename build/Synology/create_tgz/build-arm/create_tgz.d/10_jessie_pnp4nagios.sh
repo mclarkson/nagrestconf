7583afdabb62a5fa0624bf7d93652a4013e56512
Mark Clarkson <mark.clarkson@smorg.co.uk>
2015-09-11 21:56:08 +0100

    Jessie build mods

--- build/Synology/create_tgz/build-arm/create_tgz.d/10_jessie_pnp4nagios.sh
+++ build/Synology/create_tgz/build-arm/create_tgz.d/10_jessie_pnp4nagios.sh
@@ -20,5 +20,7 @@ EnD
 run "apt-get update"
 run "apt-get install -qy pnp4nagios"
 
+sed 's#AuthUserFile.*#AuthUserFile /etc/nagios3/htpasswd.users#' \
+        $ROOTNAGIOSDIR/etc/apache2/conf-available/pnp4nagios.conf
 }
 
