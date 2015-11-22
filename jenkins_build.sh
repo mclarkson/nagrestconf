0c210ffa4313ee24443f304867e9c1f0d856c041
Mark Clarkson <mark.clarkson@smorg.co.uk>
2015-11-22 16:21:11 +0000

    Fixes

--- jenkins_build.sh
+++ jenkins_build.sh
@@ -11,10 +11,8 @@ BASE=$PWD
 
 # Release Version - edits the spec file
 POINTRELEASE=`head -1 ./SOURCES/nagrestconf-1/debian.jessie/changelog | sed 's/^.*(1\.//;s/).*//'`
-echo "Package Release: $RELEASE"
-echo "New Version No.: $VERSION.${POINTRELEASE}"
-sed "s/^%define *version.*/%define version ${VERSION}.${POINTRELEASE}/g" \
-SPECS/${PKG} > TMP/${PKG}
+echo "New Version No.: 1.${POINTRELEASE}"
+sed "s/^%define *version.*/%define version 1.${POINTRELEASE}/g" SPECS/nagrestconf.spec > TMP/nagrestconf.spec
 
 SYSMACROS="`rpm --showrc | grep macrofiles | \
             sed 's/^macrofiles[ ]*:[ ]*//ig'`"
@@ -35,4 +33,4 @@ EOM
 pushd SOURCES/
 tar cvzf nagrestconf-1.tar.gz nagrestconf-1
 popd
-rpmbuild --define "_topdir `pwd`" --define "dist $dist" -ba SPECS/nagrestconf.spec
+rpmbuild --define "_topdir `pwd`" --define "dist $dist" --rcfile $BASE/TMP/rpmrc -ba $BASE/TMP/nagrestconf.spec
